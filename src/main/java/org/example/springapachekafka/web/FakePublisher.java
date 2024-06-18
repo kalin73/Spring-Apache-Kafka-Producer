a2ce0d871c8d7196e5400dfd124e26792d3fd48e
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-06-19 01:41:03 +0300

    commit

--- src/main/java/org/example/springapachekafka/web/FakePublisher.java
+++ src/main/java/org/example/springapachekafka/web/FakePublisher.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
