df7c44b5e35bc18c965cd2be3632e213683d033a
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-06-18 17:04:41 +0300

    init commit

---
+++ src/test/java/org/example/springapachekafka/SpringApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApacheKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
